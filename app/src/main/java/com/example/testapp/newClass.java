a2d0e403ea479b2b43d184eb302fe7cbad22da6c
Ant017 <antika.noor98@gmail.com>
2021-11-18 09:29:06 +0600

    java class added in master

---
+++ app/src/main/java/com/example/testapp/newClass.java
@@ -0,0 +1,16 @@
+package com.example.testapp;
+
+public class newClass {
+
+    int a;
+    public newClass() {
+    }
+
+    public int getA() {
+        return a;
+    }
+
+    public void setA(int a) {
+        this.a = a;
+    }
+}
