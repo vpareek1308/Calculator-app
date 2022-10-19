a76e251216c6d4234bb939ffb87cc0a37e14d788
Pawandeep Suryavanshi <56802662+PawanSuryavanshi95@users.noreply.github.com>
2022-10-19 21:27:44 +0530

    unit testing

--- app/src/test/java/com/example/testapp/ExampleUnitTest.java
+++ app/src/test/java/com/example/testapp/ExampleUnitTest.java
@@ -1,17 +1,18 @@
-package com.example.testapp;
-
+package project.testcompany.com.mytestapplication;
 import org.junit.Test;
-
 import static org.junit.Assert.*;
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
- */
 public class ExampleUnitTest {
     @Test
-    public void addition_isCorrect() {
-        assertEquals(4, 2 + 2);
+    public void add_isCorrect() throws Exception {
+        assertEquals(4, Utils.addNumbers(2 , 2));
     }
+    @Test
+    public void add_twodigits_isCorrect() throws Exception {
+        assertEquals(44, Utils.addNumbers(22 , 22));
+    }
+    @Test
+    public void add_big_isCorrect() throws Exception {
+        assertEquals(4444, Utils.addNumbers(2222 , 2222));
+    }
+    
 }
